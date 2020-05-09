07e3d563aaba3a6d927682cd13e6606bc9cf6dc0
Jason Heap <jasonrheap@gmail.com>
2020-05-09 13:27:24 -0600

    test2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
-import React from "react";
-import Main from "./pages/Main";
+  
+import React, { Component } from 'react';
+import Navbar from "../src/components/Navbar";
+import Footer from "../src/components/Footer";
+import Card from "../src/components/Card"
+import Portfolio from "../src/pages/Portfolio";
+import Contact from "../src/pages/Contact";
+import M from "materialize-css";
+import Skills from '../src/pages/Skills';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
-function App() {
-  return (
-    <Main />
-  );
+export default class App extends Component {
+    state = {
+        skills: "",
+        contact: "",
+        portfolio: "",
+
+    }
+    componentDidMount() {
+        M.AutoInit();
+    }
+    
+    navClick = event => {
+        this.setState({
+            skills: "",
+            contact: "",
+            portfolio: "",
+            [event.target.name]: "active"
+        })
+    }
+    render() {
+        return(
+            <Router>
+                <Navbar
+                navClick={this.navClick}
+                skills={this.state.skills}
+                contact={this.state.contact}
+                portfolio={this.state.contact}
+                />         
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <Card/>
+                        </Route>
+                        <Route path="/skills">
+                            <Skills/>
+                        </Route>
+                        <Route exact path="/contact">
+                            <Contact/>
+                        </Route>
+                        <Route path="/portfolio">
+                            <Portfolio/>
+                        </Route>
+                    </Switch>
+                </main>
+                <Footer/>
+            </Router>
+        )
+    }
 }
 
-export default App;
+
+
+
+// import React from "react";
+// import Main from "./pages/Main";
+
+
+// function App() {
+//   return (
+//     <Main />
+//   );
+// }
+
+// export default App;
